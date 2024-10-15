7c624bf81e8f467ce0efb8740f5c7067fd6dcc32
Jordan Barnes-Hand <112431791+ikori-meimoto@users.noreply.github.com>
2024-10-15 11:35:45 -0500

    fileParser is done, it's amazing!

--- fileParser.py
+++ fileParser.py
@@ -1,5 +1,8 @@
 from html.parser import HTMLParser
 
+# To use this, replace fileName with the name of the HTML file you want to parse string from.
+# The output file is called output.txt
+
 class MyHTMLParser(HTMLParser):
     # def handle_starttag(self, tag, attrs):
     #     # print("Encountered a start tag:", tag)
@@ -9,9 +12,15 @@ class MyHTMLParser(HTMLParser):
 
     def handle_data(self, data):
         print("Encountered some data  :", data)
+        ftwo.write(data)
 
-f = open("Legend of the Paladins_ The Forge - Special Channels - special-rp [541292593695293470].html", "r")
+fileName = "Legend of the Paladins_ The Forge - Special Channels - special-rp [541292593695293470].html"
+output = "output.txt"
+
+f = open(fileName, "r")
+ftwo = open(output, "a")
 parser = MyHTMLParser()
 
 for x in f:
     parser.feed(x)
+    
